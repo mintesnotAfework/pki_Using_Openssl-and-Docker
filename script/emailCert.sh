809ed578b933fb56013c4005b5bb66b2e9ce3994
mintesnot afework <mintesnontafework12@gmail.com>
2024-11-07 09:28:43 +0300

    bug fix

--- script/emailCert.sh
+++ script/emailCert.sh
@@ -8,9 +8,10 @@ mkdir ./simple-ca/$file_name
 cd ./simple-ca/$file_name
 
 mkdir private cert etc
-cp ../email-server/etc/email-server.conf etc/email-server.conf
+cp ../email-server/etc/email-server.conf etc/$file_name.conf
 
-openssl req -new -config etc/email-server.conf -out ../issuing-ca/csr/$file_name.csr -keyout private/$file_name.key
+
+openssl req -new -config etc/$file_name.conf -out ../issuing-ca/csr/$file_name.csr -keyout private/$file_name.key
 
 cd ../issuing-ca/
 openssl ca -config etc/issuing-ca.conf -in csr/$file_name.csr -out ../email-server/cert/$file_name.crt -extensions email_ext
--- script/tlsCert.sh
+++ script/tlsCert.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+
+
 echo -n "Enter the Name: "
 read file_name
 mkdir ./simple-ca/$file_name
 cd ./simple-ca/$file_name
 
 mkdir private cert etc
-cp ../web-server/etc/web-server.conf etc/web-server.conf
-cd ..openssl req -new -config etc/web-server.conf -out ../issuing-ca/csr/$file_name.csr -keyout private/$file_name.key
+cp ../web-server/etc/web-server.conf etc/$file_name.conf
 
+
+openssl req -new -config etc/$file_name.conf -out ../issuing-ca/csr/$file_name.csr -keyout private/$file_name.key
 
 cd ../issuing-ca/
 openssl ca -config etc/issuing-ca.conf -in csr/$file_name.csr -out ../$file_name/cert/$file_name.crt -extensions server_ext
